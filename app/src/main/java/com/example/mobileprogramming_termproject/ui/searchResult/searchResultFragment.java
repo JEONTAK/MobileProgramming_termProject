e06898256a237006df3c021d4f868b87cad64040
Jeon Tak <tak980418@naver.com>
2021-04-07 23:21:42 +0900

    JeonTak commit

--- app/src/main/java/com/example/mobileprogramming_termproject/ui/searchResult/searchResultFragment.java
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/searchResult/searchResultFragment.java
@@ -12,5 +12,6 @@ public class searchResultFragment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search_result);
+
     }
 }
