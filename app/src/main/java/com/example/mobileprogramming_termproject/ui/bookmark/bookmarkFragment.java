73075b22fa9e4aabe57cb3b4262e36121229d6b3
Jeon Tak <tak980418@naver.com>
2021-04-07 23:16:49 +0900

    전탁 중간

---
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/bookmark/bookmarkFragment.java
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming_termproject.ui.bookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileprogramming_termproject.R;
+
+public class bookmarkFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_bookmark);
+    }
+}
---
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/recipe/recipePagerFragment.java
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming_termproject.ui.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileprogramming_termproject.R;
+
+public class recipePagerFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_recipe_pager);
+    }
+}
