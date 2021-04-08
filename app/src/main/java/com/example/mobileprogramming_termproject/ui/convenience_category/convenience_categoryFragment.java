646cd542f431cc96f2102c656408501bb5823349
Jeon Tak <tak980418@naver.com>
2021-04-08 19:22:41 +0900

    JeonTak commit

---
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/convenience_category/convenience_categoryFragment.java
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming_termproject.ui.convenience_category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileprogramming_termproject.R;
+
+public class convenience_categoryFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_convenience_category);
+    }
+}
