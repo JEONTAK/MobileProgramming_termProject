b077c2f9f5dae47964171d0838792a56c4d60cf6
Jeon Tak <tak980418@naver.com>
2021-04-24 23:03:51 +0900

    JeonTak commit

--- app/src/main/java/com/example/mobileprogramming_termproject/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_termproject/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +27,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+    }
 }
--- app/src/main/java/com/example/mobileprogramming_termproject/ui/community/communityFragment.java
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/community/communityFragment.java
@@ -1,32 +1,66 @@
 package com.example.mobileprogramming_termproject.ui.community;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mobileprogramming_termproject.MainActivity;
 import com.example.mobileprogramming_termproject.R;
 
 public class communityFragment extends Fragment {
 
     private communityViewModel  communityViewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         communityViewModel =
                 new ViewModelProvider(this).get(communityViewModel.class);
         View root = inflater.inflate(R.layout.fragment_community, container, false);
-//        final TextView textView1 = root.findViewById(R.id.community);
-//        communityViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView1.setText(s);
-//            }
-//        });
+
+
+        LinearLayout default_layout = (LinearLayout) root.findViewById(R.id.default_community);
+        LinearLayout addWriting_layout = (LinearLayout) root.findViewById(R.id.add_layout);
+        LinearLayout writing_layout = (LinearLayout) root.findViewById(R.id.writingPlace);
+        default_layout.setVisibility(View.VISIBLE);
+        writing_layout.setVisibility(View.GONE);
+        root.findViewById(R.id.addWritingBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                default_layout.setVisibility(View.GONE);
+                writing_layout.setVisibility(View.VISIBLE);
+                addWriting_layout.setVisibility(View.GONE);
+
+            }
+        });
+
+        root.findViewById(R.id.goBackBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                default_layout.setVisibility(View.VISIBLE);
+                writing_layout.setVisibility(View.GONE);
+                addWriting_layout.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+       // final TextView textView1 = root.findViewById(R.id.community);
+       // communityViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+       //     @Override
+       //     public void onChanged(@Nullable String s) {
+       //         textView1.setText(s);
+       //     }
+       // });
+
         return root;
     }
 }
