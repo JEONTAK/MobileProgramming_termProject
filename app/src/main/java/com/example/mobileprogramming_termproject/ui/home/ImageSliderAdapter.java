a6ac8fcfbecd75f236192496dc4e9c4018df906b
woooon79 <hannah1258@naver.com>
2021-05-22 18:03:40 +0900

    광고 이미지 슬라이드..예시

--- app/src/main/java/com/example/mobileprogramming_termproject/ui/home/ImageSliderAdapter.java
+++ app/src/main/java/com/example/mobileprogramming_termproject/ui/home/ImageSliderAdapter.java
@@ -1,6 +1,9 @@
 package com.example.mobileprogramming_termproject.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +17,7 @@ import com.bumptech.glide.Glide;
 import com.example.mobileprogramming_termproject.R;
 
 public class ImageSliderAdapter extends PagerAdapter {
-   private int[] images={R.drawable.ad_coffee,R.drawable.ad_diet,R.drawable.ad_cu,R.drawable.ad_cu};
+   private int[] images={R.drawable.ad_cu,R.drawable.ad_seven,R.drawable.ad_gs,R.drawable.ad_emart,R.drawable.ad_coffee,R.drawable.ad_diet};
    private LayoutInflater inflater;
    private Context context;
 
@@ -35,6 +38,34 @@ public class ImageSliderAdapter extends PagerAdapter {
       View view=inflater.inflate(R.layout.item_slider,container,false);
       ImageView imageView=view.findViewById(R.id.imageSlider);
       imageView.setImageResource(images[position]);
+
+      view.setOnClickListener(new View.OnClickListener(){
+         public void onClick(View v){
+            //this will log the page number that was click
+            Log.i("TAG", "This page was clicked: " + position);
+            if(position==0){
+
+               Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://cu.bgfretail.com/index.do"));
+               context.startActivity(intent);
+            }
+            else if(position==1){
+
+               Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.7-eleven.co.kr/"));
+               context.startActivity(intent);
+            }
+            else if(position==2){
+
+               Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://gs25.gsretail.com/gscvs/ko/main"));
+               context.startActivity(intent);
+            }
+            else if(position==4){
+
+               Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://emart24.co.kr/"));
+               context.startActivity(intent);
+            }
+         }
+      });
+
       container.addView(view);
       return view;
    }
